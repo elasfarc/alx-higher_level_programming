fcfec6a7ed64b69cde27ae31dd1de71e051a1fc1
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-09 10:12:55 +0300

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if (num % 3 == 0 or num % 5 == 0):
+            if num % 3 == 0:
+                print("Fizz", end="")
+            if num % 5 == 0:
+                print("Buzz", end="")
+        else:
+            print({}.format(num), end="")
+        print(" ", end="")
