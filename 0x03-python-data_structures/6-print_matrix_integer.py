07cef9d7efe421cb3adcd116f4f81af0bd358c16
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-10 05:51:44 +0300

    6-print_matrix_integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i < len(row) - 1:
-                print("{}".format(row[i]), end=" ")
-            else:
-                print("{}".format(row[i]))
+        if len(row) > 0:
+            for i in range(len(row)):
+                if i < len(row) - 1:
+                    print("{}".format(row[i]), end=" ")
+                else:
+                    print("{}".format(row[i]))
+        else:
+            print()
