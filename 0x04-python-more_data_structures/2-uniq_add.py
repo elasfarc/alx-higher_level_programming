2851d0ec1fffab038794d798954ff34a5d390b9b
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-12 01:56:14 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,7 +6,6 @@ def reduce(reducer, seq, init):
         accum = reducer(accum, element)
     return accum
 
-def uniq_add(my_list=[]):
-    _sum = lambda n1, n2: n1 + n2
-    return reduce(_sum, set(my_list), 0)
 
+def uniq_add(my_list=[]):
+    return reduce(lambda n1, n2: n1 + n2, set(my_list), 0)
