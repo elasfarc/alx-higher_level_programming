e38458b577c971becff9128ebdaf976751dc2f3f
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-12 04:15:23 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
     roman_nums = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     subcase = dict(I=('X', 'V'), X=('C', 'L'), C=('M', 'D'))
     num, idx, length = 0, 0, len(roman_string)
