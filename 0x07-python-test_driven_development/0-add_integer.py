a78f19358bbd658e4088dbfab45ec8c9add99d2e
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-29 05:26:17 +0200

    pystyle for 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
-def add_integer(a:int, b=98):
-	are_type = [n == n and type(n) in [float, int] for n in [a, b]]
-	if not all(are_type):
-		raise TypeError(f"{'a' if not are_type[0] else 'b'} must be an integer")
-	return int(a) + int(b)
+def add_integer(a: int, b=98):
+    are_type = [n == n and type(n) in [float, int] for n in [a, b]]
+    if not all(are_type):
+        raise TypeError(
+            f"{'a' if not are_type[0] else 'b'} must be an integer"
+        )
+    return int(a) + int(b)
