a4aca09c2d0bc61877d17983d16ed957df3a31a6
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-29 09:08:09 +0200

    add documetation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+"""Defines a matrix division function."""
+
+
 def add_integer(a: int, b=98):
+    """Return the integer addition of a and b.
+
+        Float arguments are typecasted to ints before addition is performed.
+
+        Raises:
+            TypeError: If either of a or b is a non-integer and non-float.
+        """
     are_type = [n == n and type(n) in [float, int] for n in [a, b]]
     if not all(are_type):
         raise TypeError(
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,17 @@
+#!/usr/bin/python3
+"""Defines say_my_name function."""
+
+
 def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+        Args:
+            first_name (str): The first name to print.
+            last_name (str): The last name to print.
+        Raises:
+            TypeError: If either of first_name or last_name are not strings.
+
+    """
     valid_args = [type(name) is str for name in [first_name, last_name]]
     if not all(valid_args):
         raise TypeError("{} must be a string".format(
