80c08bcb6e480cea84bbaf573583c7fcbe35508d
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-31 02:58:37 +0200

    1-rectangle.py

--- 0x08-python-more_classes/test_rectangle.py
+++ 0x08-python-more_classes/test_rectangle.py
@@ -1,12 +1,56 @@
 import unittest
-Rectangle = __import__("0-rectangle").Rectangle
+Rectangle = __import__("1-rectangle").Rectangle
 
 
 class test_rectangle(unittest.TestCase):
 
+    def setUp(self):
+        self.rec = Rectangle()
+
     def test_creating_instances(self):
-        rec = Rectangle()
-        self.assertIsInstance(rec, Rectangle)
+        self.assertIsInstance(self.rec, Rectangle)
+
+    def test_triangle_width(self):
+        rec = self.rec
+        with self.assertRaises(AttributeError):
+            rec.__width
+        rec.width = 5
+        self.assertEqual(rec.width, 5)
+
+        rec2 = Rectangle(12)
+        self.assertEqual(rec2.width, 12)
+        rec2.width = 122
+        self.assertEqual(rec2.width, 122)
+
+        with self.assertRaises(TypeError) as e:
+            Rectangle(7.5)
+
+        self.assertEqual(str(e.exception), "width must be an integer")
+
+        with self.assertRaises(ValueError) as e:
+            Rectangle(-4)
+        self.assertEqual(str(e.exception), "width must be >= 0")
+
+    def test_triangle_height(self):
+        rec = self.rec
+        with self.assertRaises(AttributeError):
+            rec.__height
+        rec.height = 5
+        self.assertEqual(rec.height, 5)
+
+        rec2 = Rectangle(12)
+        self.assertEqual(rec2.height, 0)
+        rec2.height = 122
+        self.assertEqual(rec2.height, 122)
+
+        with self.assertRaises(TypeError) as e:
+            Rectangle(0, 7.5)
+
+        self.assertEqual(str(e.exception), "height must be an integer")
+
+        with self.assertRaises(ValueError) as e:
+            Rectangle(0, -4)
+        self.assertEqual(str(e.exception), "height must be >= 0")
 
 
 if __name__ == "__main__":
