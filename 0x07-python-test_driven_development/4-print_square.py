51b84bd35cc29d99f4776794dbe30e4d8f69cc8b
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-11-03 18:42:22 +0200

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,7 +14,7 @@ def print_square(size: int):
     if not type(size) is int:
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be >=0")
+        raise ValueError("size must be >= 0")
 
     for i in range(int(size)):
         for _ in range(int(size)):
