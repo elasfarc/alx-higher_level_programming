cca448c4fb3c100d507d00287c9b3c15dd5e0fab
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-11-07 00:11:22 +0200

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+defines derived class square.
+"""
+
+Rectangle: type = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class represents a Square, a derived class from Rectangle.
+
+    Attributes:
+        - __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square instance with the specified size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            ValueError: If size is not a positive integer.
+        """
+        Rectangle.__init__(self, size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the Square.
+
+        Returns:
+            int: The area of the Square (product of size by itself).
+        """
+        return self.__size * self.__size
