dc0c969c2d66655801e3cb9963d98a346c44eb99
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-11-06 23:00:07 +0200

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""6-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry
+    """
+    def area(self):
+        """Public instance method area
+        Raises: Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
