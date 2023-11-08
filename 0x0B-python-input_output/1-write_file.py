f72d5e7c18ce5635c039f0a118c6ae250b68f23b
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-11-09 01:07:46 +0200

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,15 @@
 
 
 def write_file(filename="", text=""):
+    """
+    a function that writes a string to a text file (UTF8)
+    Args:
+        @filename: name of the file to be created/overwritten
+        @text: text to be written to the file
+
+    Return:
+         the number of characters written
+
+    """
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
