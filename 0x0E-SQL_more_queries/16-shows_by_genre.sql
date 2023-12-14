a4ec145b606a54347127938bb757f17fcd51e46a
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-12-14 05:00:40 +0200

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@
 
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-left JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-left JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY title, name;
