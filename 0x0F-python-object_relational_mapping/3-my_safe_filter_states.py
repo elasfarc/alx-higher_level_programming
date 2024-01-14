e69d71b9cd57a37412af7f12b4401b802b2783d9
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-01-14 21:39:23 +0200

    3. SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that takes in an argument and displays all values in the states
+  table of hbtn_0e_0_usa where name matches the argument.
+  sorted in ascending order by states.id
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    (_, SQL_USR, SQL_PASSWD, DB_NAME, search) = sys.argv
+
+    db = MySQLdb.connect(
+      host="localhost", user=SQL_USR, password=SQL_PASSWD,
+      database=DB_NAME, port=3306)
+
+    cur = db.cursor()
+    cur.execute("""
+      SELECT * from states
+      WHERE name like BINARY (%s)
+      ORDER BY id
+      """, (search,))
+    results = cur.fetchall()
+    for res in results:
+        print(res)
+
+    cur.close()
+    db.close()
