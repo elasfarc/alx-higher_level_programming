8f0202ed198daa4f765afcf2946f13ad84cd6e0d
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-01-14 21:08:21 +0200

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""A script that lists all states from the database hbtn_0e_0_usa
+  with a name starting with N
+  sorted in ascending order by states.id
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    (_, SQL_USR, SQL_PASSWD, DB_NAME) = argv
+
+    db = MySQLdb.connect(
+      host="localhost", user=SQL_USR, password=SQL_PASSWD,
+      database=DB_NAME, port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * from  states WHERE name LIKE 'N%' ORDER BY id")
+    results = cur.fetchall()
+
+    for res in results:
+        print(res)
+
+    cur.close()
+    db.close()
