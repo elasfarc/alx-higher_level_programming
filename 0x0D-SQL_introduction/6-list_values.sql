b64f517584fc51e2af78788aca804b7d9c01f41a
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-05-21 19:55:20 -0500

    Create 6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+-- select all fields from the table first_table
+SELECT * from first_table;
