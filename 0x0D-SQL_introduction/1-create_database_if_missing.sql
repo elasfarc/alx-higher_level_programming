3bd7b10ea53caf36917e10ee3d821e342e4fcbdd
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-05-21 19:03:51 -0500

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
 
 -- create the database hbtn_0c_0 if it does not exist
-CREATE DATABASE IF NOT EXIST hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
