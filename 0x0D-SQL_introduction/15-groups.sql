447b6bbbb426cd4be66197c4fad0def33372f983
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-05-21 21:47:29 -0500

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,7 +4,6 @@
 -- the score
 -- the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
-
 SELECT score, count(score) AS number
 FROM second_table
 GROUP BY score;
